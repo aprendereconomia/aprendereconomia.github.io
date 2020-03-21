b34208ba5c19477176c82d96b31ea35f868c2280
José Tobias <jose.tobias@outlook.com>
2020-03-21 20:04:37 -0300

    feat: test score is now being shown
    Signed-off-by: José Tobias <jose.tobias@outlook.com>

--- assets/js/questions_load.js
+++ assets/js/questions_load.js
@@ -1,9 +1,9 @@
 var right_classes = ["bg-success", "text-white"];
+var wrong_classes = ["bg-danger", "text-white"];
+var total_answers = 0;
 var correct_answers = 0;
 
 function checkLimit(comp, limit) {
-   console.log(comp.value);
-
    if (!comp.checked) {
       return;
    }
@@ -27,48 +27,74 @@ function resetAnswer(size) {
          comp[j].disabled = false;
          comp[j].checked = false;
          right_classes.forEach(e => $(comp[j]).next().removeClass(e));
+         wrong_classes.forEach(e => $(comp[j]).next().removeClass(e));
       }
    }
-   $("#reset-answer").css("display", "none");
+   buttonsManagement(false);
+}
 
+function manageCheckbox(comp) {
+   var accepted = 0;
+   var right = 0;
+   var [is_right, accepted] = comp[0].value.split(",").map(x => parseInt(x));
+   total_answers += accepted;
+   for (var j = 0; j < comp.length; j++) {
+      var is_right = parseInt(comp[j].value.split(",")[0]);
+      var choice = comp[j];
+      choice.disabled = true;
+      if (choice.checked) {
+         if (is_right === 1) {
+            right_classes.forEach(e => $(choice).next().addClass(e));
+            ++correct_answers;
+            ++right;
+            if (right == accepted) {
+               break;
+            }
+         } else {
+            wrong_classes.forEach(e => $(choice).next().addClass(e));
+         }
+      }
+   }
+}
+
+function manageRadio(comp) {
+   ++total_answers;
+   for (var j = 0; j < comp.length; j++) {
+      var choice = comp[j];
+      choice.disabled = true;
+      if (choice.checked) {
+         if (choice.value == "1") {
+            ++correct_answers;
+            right_classes.forEach(e => $(choice).next().addClass(e));
+         } else {
+            wrong_classes.forEach(e => $(choice).next().addClass(e));
+         }
+      }
+   }
 }
 
 function checkAnswer(size) {
    for (var i = 1; i <= size; i++) {
       var comp = $("input[name=Q-" + i + "]");
-      var accepted = 0;
-      var right = 0;
       if (comp[0].type == "checkbox") {
-         accepted = comp[0].value.split(",")[1];
-         console.log("accepted = " + accepted);
-         for (var j = 0; j < comp.length; j++) {
-            var choice = comp[j];
-            choice.disabled = true;
-            right_classes.forEach(e => $(choice).next().removeClass(e));
-            if (choice.checked) {
-               var is_right = choice.value.split(",")[0];
-               if (is_right == "1") {
-                  right_classes.forEach(e => $(choice).next().addClass(e));
-                  console.log(choice);
-                  right += 1;
-                  if (right == accepted) {
-                     break;
-                  }
-               }
-            }
-         }
+         manageCheckbox(comp);
       } else if (comp[0].type == "radio") {
-         for (var j = 0; j < comp.length; j++) {
-            var choice = comp[j];
-            choice.disabled = true;
-            right_classes.forEach(e => $(choice).next().removeClass(e));
-            if (choice.checked) {
-               if (choice.value == "1") {
-                  right_classes.forEach(e => $(choice).next().addClass(e));
-               }
-            }
-         }
+         manageRadio(comp);
       }
    }
-   $("#reset-answer").css("display", "");
+   buttonsManagement(true);
+}
+
+function buttonsManagement(is_send) {
+   if (is_send) {
+      $("#results").append("<p class='card card-body bg-warning my-2' style='width: 18rem;''>Pontuação: " + correct_answers + " de " + total_answers + "</p>");
+      $("#results").css("display", "");
+      $("#reset-answer").css("display", "");
+      $("#send-answer").css("display", "none");
+   } else {
+      total_answers = correct_answers = 0;
+      $("#reset-answer").css("display", "none");
+      $("#send-answer").css("display", "");
+      $("#results").empty();
+   }
 }
