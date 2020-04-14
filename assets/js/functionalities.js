1ef0bbd2489ce9ca4979e3d2b1e3841398da85f4
José Tobias <jose.tobias@outlook.com>
2020-04-14 14:03:16 -0300

    fix: files format and variables names
    Signed-off-by: José Tobias <jose.tobias@outlook.com>

--- assets/js/functionalities.js
+++ assets/js/functionalities.js
@@ -5,3 +5,10 @@ $('.videoModal').on('hide.bs.modal', function (e) {
     $if.attr("src", src);
 });
 
+// (() => {
+//     el = $(".presentationModal");
+//     console.log(/Mobi/.test(navigator.userAgent));
+//     if (!(/Mobi/.test(navigator.userAgent))) {
+//         el.children().find(".modal-footer").append("<a class='btn btn-primary text-white' href='{{ site.baseurl }} /{{ pres.src }}' download='{{ pres.src }}'>Baixar apresentação</a >");
+//     }
+// })();
