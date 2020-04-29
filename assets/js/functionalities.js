b57bd46811d2755c53c176a3daae1edfac58c1cd
José Tobias <jose.tobias@outlook.com>
2020-04-29 12:56:04 -0300

    feat: add filter box for topics
    Signed-off-by: José Tobias <jose.tobias@outlook.com>

--- assets/js/functionalities.js
+++ assets/js/functionalities.js
@@ -5,10 +5,20 @@ $('.videoModal').on('hide.bs.modal', function (e) {
     $if.attr("src", src);
 });
 
-// (() => {
-//     el = $(".presentationModal");
-//     console.log(/Mobi/.test(navigator.userAgent));
-//     if (!(/Mobi/.test(navigator.userAgent))) {
-//         el.children().find(".modal-footer").append("<a class='btn btn-primary text-white' href='{{ site.baseurl }} /{{ pres.src }}' download='{{ pres.src }}'>Baixar apresentação</a >");
-//     }
-// })();
+function filter_list(e) {
+    let element_text = $(e).val();
+
+    let search_text = element_text.normalize("NFC").toLowerCase().trim();
+    let search_pattern = new RegExp(search_text, 'i');
+    let list = $(e).parent().parent().next(".list-group").children(".list-group-item");
+
+    list.each(function (idx, el) {
+        let text = $(el).text().normalize("NFC").toLowerCase().trim();
+
+        if (search_pattern.exec(text)) {
+            $(el).css("display", "inline");
+        } else {
+            $(el).css("display", "none");
+        }
+    });
+}
